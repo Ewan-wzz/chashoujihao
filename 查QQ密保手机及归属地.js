4f9cb274f4b75c877c42d5b8223440a8d40a8be5
Ewan-wzz <119650287+Ewan-wzz@users.noreply.github.com>
2023-03-23 11:18:08 +0800

    @回复用户

--- 查QQ密保手机及归属地.js
+++ 查QQ密保手机及归属地.js
@@ -39,11 +39,7 @@ export class chashoujihao extends plugin {
             let url = `https://zy.xywlapi.cc/qqapi?qq=${i}`;
             let response = await fetch(url);
             let res = await response.json();
-            let message = `QQ:${i}\n` +
-                `查询状态：${segment.text(res.message)}\n` +
-                `绑定的号码:${segment.text(res.phone)}\n` +
-                `地区:${segment.text(res.phonediqu)}\n` +
-                `不要做坏事哦!`;
+            let message = `QQ:${i}\n`+`查询状态：${segment.text(res.message)}\n` +`绑定的号码:${segment.text(res.phone)}\n`+`地区:${segment.text(res.phonediqu)}\n`+`不要做坏事哦!`;
              await e.reply(`@${e.user_id}`, message);
         }
         return true; //返回true阻挡消息不再往下
